db790e80bb974b1d9695a32a26283d4b825b10c6
Sikharin Kitchpanich <bankster007@gmail.com>
2018-10-18 13:39:23 -0800

    Commented a lot of things, added some validation

--- players/views.py
+++ players/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from .forms import BetForm, PlayerForm
 from accounts.models import Profile
 
-#TODO: Test printing errors or request.user to a template
+#TODO: Get player_by_id out to the template somehow
 
 def index(request):
 	return HttpResponse("<h1>test</h1>")
@@ -48,18 +48,24 @@ def betminus(request):
 			return HttpResponseRedirect(reverse('index'))
 
 def challenge(request):
-#User works by calling get(username='')
+# User works by calling get(username='')
 
 	if request.method == 'POST':
-
+		current_player = Profile.objects.get(user=request.user)
 		player_form = PlayerForm(request.POST)
 
 		if player_form.is_valid():
-			#['player'] field has to match what's in the index template's form name.  BULLSHIT!
+			# ['player'] field has to match what's in the index template's form name.  BULLSHIT!
+			# This is responsible for getting the POST data btw.
 			name = player_form.cleaned_data['player']
-			player_object = User.objects.get(username=name)
+			# player_object grabs the user from the User class using the username provided by the POST data.
+			try:
+				player_object = User.objects.get(username__iexact=name)
+			except User.DoesNotExist:
+				return HttpResponse("My dude that person isn't a player!")
+			# Getting the primary key of the pulled user.
 			ky = player_object.id
-			#seems like this method works right meow?
+			# I can't pass in the user directly for some fucking reason so it's gotta be done this way GRRR D:<
 			player_by_id = Profile.objects.get(user=ky)
 			return HttpResponse(player_by_id)
 
