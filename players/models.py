f477f524765c9ca58de9e67e41ddf092c573a5c5
Sikharin Kitchpanich <bankster007@gmail.com>
2018-10-26 14:37:15 -0800

    Starting to work on challenge feature
    Created a challenge table

--- players/models.py
+++ players/models.py
@@ -2,15 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse
 
-class Stats(models.Model):
-	player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	munny = models.IntegerField(default=1000)
-	wins = models.IntegerField(default=0)
-	losses = models.IntegerField(default=0)
-	matches = models.IntegerField(default=0)
-
 class Challenge(models.Model):
-	sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	munny = models.IntegerField(default=0)
+	sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="sender")
+	receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="receiver")
+	bet = models.IntegerField(default=0)
+	status = models.IntegerField(default=0)
+	result = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null=True, related_name="winner")
 
